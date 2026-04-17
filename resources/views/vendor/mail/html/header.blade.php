3f45affbf5f1e6e1877461d2f274d8a41a064894
Maria Jose Novoa <nmariajose7@gmail.com>
2026-04-17 11:11:24 -0400

    Fix: correo de verificacion y limpieza del header
    cambios para el mensaje de verificar correo en español, ya que la imagen del logo no se lee porque no es de un directorio público.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,9 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/dimak-logo.png') }}" alt="{{ config('app.name') }}"
-                style="height: 60px; max-width: 200px;">
+            <div style="margin-top: 10px; font-weight: bold; font-size: 18px;">
+                {{ config('app.name') }}
+            </div>
         </a>
     </td>
 </tr>
