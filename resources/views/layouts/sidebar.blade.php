06c53f907f291da8ccad54759e9c0626b136152e
Maria Jose Novoa <nmariajose7@gmail.com>
2026-05-11 17:24:46 -0400

    Update sidebar.blade.php
    redirección actual de Dashboard de vehiculos

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -78,7 +78,7 @@
                     class="space-y-1 bg-gray-800/50 mt-1 rounded-md overflow-hidden">
 
                     @if(in_array(Auth::user()->role, ['supervisor', 'viewer']))
-                        <a href="{{ route('dashboard') }}" wire:navigate
+                        <a href="{{ route('vehicles.dashboard') }}" wire:navigate
                             class="flex items-center pl-11 pr-2 py-2 text-sm text-gray-400 rounded-md hover:text-white hover:bg-gray-800"
                             :class="{{ request()->routeIs('vehicles.dashboard') ? "'text-white bg-gray-800'" : "''" }}">
                             Panel de Vehículos
